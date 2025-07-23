40899dd649541608469dae38305d1f547ca56d14
hws <hwonsuk11@gmail.com>
2025-07-24 06:40:58 +0900

    fix
    위에 것까지 이름 관련해서 수정하고 제 코드 만들어서 올렸습니다

--- review_analysis/preprocessing/main.py
+++ review_analysis/preprocessing/main.py
@@ -78,5 +78,4 @@ if __name__ == "__main__":
             preprocessor.preprocess()
             preprocessor.feature_engineering()
             preprocessor.save_to_database()
-            preprocessor.visualize()
 
