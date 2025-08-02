b162dbf98898b617ad90882476fc84e08d473f60
Sungu Kang <r191d1ty.w00@gmail.com>
2025-08-02 18:19:09 +0900

    chore: Change env name, Improve requirements.txt

--- database/mysql_connection.py
+++ database/mysql_connection.py
@@ -8,11 +8,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # 환경변수에서 데이터베이스 연결 정보 가져오기
-user = os.getenv('DB_USER', 'root')
-passwd = os.getenv('DB_PASSWORD', '')
-host = os.getenv('DB_HOST', 'localhost')
-port = os.getenv('DB_PORT', '3306')
-db = os.getenv('DB_NAME', 'user_db')
+user = os.getenv('MYSQL_USER', 'root')
+passwd = os.getenv('MYSQL_PASSWORD', '')
+host = os.getenv('MYSQL_HOST', 'localhost')
+port = os.getenv('MYSQL_PORT', '3306')
+db = os.getenv('MYSQL_NAME', 'user_db')
 
 DB_URL = f'mysql+pymysql://{user}:{passwd}@{host}:{port}/{db}?charset=utf8'
 
